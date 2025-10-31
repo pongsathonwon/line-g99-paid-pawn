bc5bcb19ac1a4ff3de30679d86993cdb67d3df0e
Suchat <suchatkraisakun3@gmail.com>
2025-10-31 16:40:02 +0700

    (refactor) styling input component

--- src/component/FormControl/FormControl.tsx
+++ src/component/FormControl/FormControl.tsx
@@ -3,17 +3,12 @@ import { FormControlContext, useFormControlContext } from "./FormControlContext"
 import type { TFormControlContext } from "./formControl.type"
 
 
-const defaultProps : Omit<TFormControlContext, 'id'> = {
-    size : "medium",
-    color: "base"
-}
-
-function FormControl({children, id, ...props}: React.PropsWithChildren<Partial<TFormControlContext>>){
+function FormControl({children, id, size = "medium", color = "base"}: React.PropsWithChildren<Partial<TFormControlContext>>){
   const fallbackId = useId();
   const validId = id ?? fallbackId
-  const contextValue = ({...defaultProps, id: validId, ...props}) 
+  const contextValue = ({id: validId, size, color}) 
   return <FormControlContext.Provider value={contextValue}>
-    <div className="flex flex-col">
+    <div className="flex flex-col w-60">
       {children}
     </div>
   </FormControlContext.Provider>
@@ -21,22 +16,22 @@ function FormControl({children, id, ...props}: React.PropsWithChildren<Partial<T
 
 type TFormLabelProps = Omit<React.LabelHTMLAttributes<HTMLLabelElement>, 'htmlFor'>
 
-const FormLabel = ({children, className,...props} : TFormLabelProps) => {
-  const {id} = useFormControlContext()
-  return <label {...props} htmlFor={`label-${id}`} >{children}</label>
+const FormLabel = ({children, className = '',...props} : TFormLabelProps) => {
+  const {id, lableClass} = useFormControlContext()
+  return <label {...props} className={`${lableClass} ${className}`} htmlFor={id} >{children}</label>
 }
 type TFormInputProps = Omit<React.InputHTMLAttributes<HTMLInputElement>, 'id'>
 
-const BaseFormInput = ({className, ...props} : TFormInputProps, ref : React.ForwardedRef<HTMLInputElement>) => {
-  const {id} = useFormControlContext()
-  return <input {...props} id={id} ref={ref} />
+const BaseFormInput = ({className = '', ...props} : TFormInputProps, ref : React.ForwardedRef<HTMLInputElement>) => {
+  const {id, inputClass} = useFormControlContext()
+  return <input {...props} className={`${inputClass} ${className}`} id={id} ref={ref} />
 }
 
 const FormInput = React.forwardRef<HTMLInputElement, TFormInputProps>(BaseFormInput)
 
 const FormErrorText = ({children}: React.PropsWithChildren) => {
-  const {} = useFormControlContext()
-  return <span>{children}</span>
+  const {id} = useFormControlContext()
+  return <span className="mb-1.5 text-sm text-red-600" role="alert" id={`${id}-error`}>{children}</span>
 }
 
 FormControl.Label = FormLabel;
