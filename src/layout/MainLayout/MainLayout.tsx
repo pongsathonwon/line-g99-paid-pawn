eadb843c106979e9fa97f6283a414197e4a671d8
Panupat.Dev <123447221+BugNoTensei@users.noreply.github.com>
2025-11-20 00:33:17 +0700

    .

--- src/layout/MainLayout/MainLayout.tsx
+++ src/layout/MainLayout/MainLayout.tsx
@@ -1,16 +1,12 @@
+import React from "react";
 import { Outlet } from "react-router-dom";
 import ProfileBadge from "@/component/ui/ProfileBadge";
 import BottomNav from "@/component/ui/BottomNav";
-import { useLineContext } from "@/context/LineContext/LineContext";
 
 function MainLayout() {
-  const { lineCtx } = useLineContext();
   return (
     <>
-      <ProfileBadge
-        displayName={lineCtx?.profile?.displayName ?? "display name here"}
-        pictureUrl={lineCtx?.profile?.pictureUrl}
-      />
+      <ProfileBadge displayName="display name here" />
       <section className="overflow-hidden px-4 sm:px-8 pt-content-top pb-content-bottom">
         <Outlet />
       </section>
