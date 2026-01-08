eb583782f71a358d01ba9bc606e59b1de956328a
pongsathonwon <men1234-2008@hotmail.com>
2026-01-08 11:21:54 +0700

    [fix] using bot qr code standard

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -33,19 +33,21 @@ function HomePage() {
       <h3 className="font-semibold text-2xl text-center mb-6 lg:text-3xl">
         รายการขายฝาก
       </h3>
-      <div className="flex flex-col gap-4 items-center w-full md:gap-8">
+      <ul className="flex flex-col gap-4 items-center w-full md:gap-8">
         {data?.map(({ pawnNumb, pawnPrice, nextPaidDate }) => (
-          <NavLink to={pawnNumb} className="w-full flex justify-center">
-            <PayCard
-              key={pawnNumb}
-              contractNumber={pawnNumb}
-              principal={pawnPrice}
-              dueDate={nextPaidDate}
-              paymentLink={pawnNumb}
-            />
-          </NavLink>
+          <li key={pawnNumb}>
+            <NavLink to={pawnNumb} className="flex justify-center">
+              <PayCard
+                key={pawnNumb}
+                contractNumber={pawnNumb}
+                principal={pawnPrice}
+                dueDate={nextPaidDate}
+                paymentLink={pawnNumb}
+              />
+            </NavLink>
+          </li>
         ))}
-      </div>
+      </ul>
     </div>
   );
 }
