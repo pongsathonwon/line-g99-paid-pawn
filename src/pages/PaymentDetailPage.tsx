66ede78fe114212ef40d4127dfa969ff75c5aec4
pongsathonwon <men1234-2008@hotmail.com>
2026-01-11 15:25:22 +0700

    [refactor] flat chain ternary statement to negative space if coditon

--- src/pages/PaymentDetailPage.tsx
+++ src/pages/PaymentDetailPage.tsx
@@ -19,9 +19,9 @@ function PaymentDetailPage() {
     return error.message ?? "เกิดข้อผิดพลาดในการโหลดข้อมูล";
   };
 
-  return (
-    <div>
-      {isError ? (
+  if (isError) {
+    return (
+      <div>
         <div className="flex flex-col items-center justify-center p-6 text-center">
           <div className="text-red-600 font-semibold text-lg mb-2">
             เกิดข้อผิดพลาด
@@ -31,52 +31,61 @@ function PaymentDetailPage() {
             <Button>กลับหน้าหลัก</Button>
           </NavLink>
         </div>
-      ) : !isSuccess || !interest ? (
-        <div>loading ....</div>
-      ) : (
-        <DisplayCard>
-          <DisplayCard.Header>สรุปการชำระ</DisplayCard.Header>
-          <DisplayCard.Subheader>รายละเอียดสัญญา</DisplayCard.Subheader>
-          <DisplayCard.Mute>
-            <span>เลขที่สัญญา</span>
-            <span>{interest.pawnNumb}</span>
-          </DisplayCard.Mute>
-          <DisplayCard.Mute>
-            <span>วันครบกำหนด</span>
-            <span>{formatThaiDate(interest.dueDate)}</span>
-          </DisplayCard.Mute>
-          <DisplayCard.Mute>
-            <span>มูลค่าจำนำ</span>
-            <span className="font-bold">{interest.pawnPrice} บาท</span>
-          </DisplayCard.Mute>
-          <DisplayCard.Mute>
-            <span>ดอกเบี้ย</span>
-            <span>{interest.interestRate} %</span>
-          </DisplayCard.Mute>
-          <DisplayCard.Divider color="gold" line="dash" />
-          <DisplayCard.Mute>
-            <span>ชำระภายใน</span>
-            <span>{formatThaiDate(interest.validBefore)}</span>
-          </DisplayCard.Mute>
-          <DisplayCard.Mute>
-            <span>ดอกเบี้ย</span>
-            <span>{interest.totalInterest.toFixed(2)} บาท</span>
-          </DisplayCard.Mute>
-          <DisplayCard.Mute>
-            <span>ส่วนลดสมาชิก</span>
-            <span>{interest.membDisc.toFixed(2)} บาท</span>
-          </DisplayCard.Mute>
-          <DisplayCard.Summary>
-            <span>ยอดชำระ</span>
-            <span>{interest.netInterest.toFixed(2)} บาท</span>
-          </DisplayCard.Summary>
-          <DisplayCard.Divider color="base" />
+      </div>
+    );
+  }
+  if (!isSuccess || !interest) {
+    return (
+      <div>
+        <div>loading ....</div>;
+      </div>
+    );
+  }
 
-          <NavLink to="./qr">
-            <Button fullWidth>ชำระดอกเบี้ย</Button>
-          </NavLink>
-        </DisplayCard>
-      )}
+  return (
+    <div>
+      <DisplayCard>
+        <DisplayCard.Header>สรุปการชำระ</DisplayCard.Header>
+        <DisplayCard.Subheader>รายละเอียดสัญญา</DisplayCard.Subheader>
+        <DisplayCard.Mute>
+          <span>เลขที่สัญญา</span>
+          <span>{interest.pawnNumb}</span>
+        </DisplayCard.Mute>
+        <DisplayCard.Mute>
+          <span>วันครบกำหนด</span>
+          <span>{formatThaiDate(interest.dueDate)}</span>
+        </DisplayCard.Mute>
+        <DisplayCard.Mute>
+          <span>มูลค่าจำนำ</span>
+          <span className="font-bold">{interest.pawnPrice} บาท</span>
+        </DisplayCard.Mute>
+        <DisplayCard.Mute>
+          <span>ดอกเบี้ย</span>
+          <span>{interest.interestRate} %</span>
+        </DisplayCard.Mute>
+        <DisplayCard.Divider color="gold" line="dash" />
+        <DisplayCard.Mute>
+          <span>ชำระภายใน</span>
+          <span>{formatThaiDate(interest.validBefore)}</span>
+        </DisplayCard.Mute>
+        <DisplayCard.Mute>
+          <span>ดอกเบี้ย</span>
+          <span>{interest.totalInterest.toFixed(2)} บาท</span>
+        </DisplayCard.Mute>
+        <DisplayCard.Mute>
+          <span>ส่วนลดสมาชิก</span>
+          <span>{interest.membDisc.toFixed(2)} บาท</span>
+        </DisplayCard.Mute>
+        <DisplayCard.Summary>
+          <span>ยอดชำระ</span>
+          <span>{interest.netInterest.toFixed(2)} บาท</span>
+        </DisplayCard.Summary>
+        <DisplayCard.Divider color="base" />
+
+        <NavLink to="./qr">
+          <Button fullWidth>ชำระดอกเบี้ย</Button>
+        </NavLink>
+      </DisplayCard>
     </div>
   );
 }
