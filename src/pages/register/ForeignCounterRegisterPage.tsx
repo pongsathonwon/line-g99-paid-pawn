775858d6e3d99147969a3d31ab5bfd3966c02b47
Panupat.Dev <123447221+BugNoTensei@users.noreply.github.com>
2026-01-11 23:50:01 +0700

    Update ForeignCounterRegisterPage.tsx

--- src/pages/register/ForeignCounterRegisterPage.tsx
+++ src/pages/register/ForeignCounterRegisterPage.tsx
@@ -1,5 +1,16 @@
+import ForeignCounterRegisterForm from "@/component/feature/RegisterForm/ForeignCounterRegisterForm";
+import { MultiStepFormContextProvider } from "@/context/MultistepFormContext/MultiStepFormContext";
+
 function ForeignCounterRegisterPage() {
-  return <div>ForeignCounterRegisterPage</div>;
+  return (
+    <div className="min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md mx-auto">
+        <MultiStepFormContextProvider totalPage={3}>
+          <ForeignCounterRegisterForm />
+        </MultiStepFormContextProvider>
+      </div>
+    </div>
+  );
 }
 
 export default ForeignCounterRegisterPage;
