b911170c9dbc544424cbde47fce8c9685a33de39
Panupat.Dev <123447221+BugNoTensei@users.noreply.github.com>
2026-01-15 01:11:46 +0700

    Update ForeignRegisterPage.tsx

--- src/pages/register/ForeignRegisterPage.tsx
+++ src/pages/register/ForeignRegisterPage.tsx
@@ -3,8 +3,10 @@ import { MultiStepFormContextProvider } from "@/context/MultistepFormContext/Mul
 
 function ForeignRegisterPage() {
   return (
+    
     <div className="min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md mx-auto">
+        
         <MultiStepFormContextProvider totalPage={4}>
           <ForeignRegisterForm />
         </MultiStepFormContextProvider>
