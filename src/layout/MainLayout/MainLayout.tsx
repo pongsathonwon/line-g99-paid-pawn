e45a2ec85cb24f08634af3cbf26964071099cd37
pongsathonwon <men1234-2008@hotmail.com>
2026-02-06 09:06:59 +0700

    [chore]

--- src/layout/MainLayout/MainLayout.tsx
+++ src/layout/MainLayout/MainLayout.tsx
@@ -1,8 +1,9 @@
 import { Outlet } from "react-router-dom";
-import ProfileBadge from "@/component/ui/ProfileBadge";
-import BottomNav from "@/component/ui/BottomNav";
+
 import { useLineContext } from "@/context/LineContext/LineContext";
 import { useAuthContext } from "@/context/AuthContext/AuthContext";
+import ProfileBadge from "@/component/Ui/ProfileBadge";
+import BottomNav from "@/component/Ui/BottomNav";
 
 function MainLayout() {
   const { lineCtx } = useLineContext();
@@ -11,15 +12,8 @@ function MainLayout() {
     "https://fastly.picsum.photos/id/962/200/200.jpg?hmac=XehF7z9JYkgC-2ZfSP05h7eyumIq9wNKUDoCLklIhr4";
   const { auth } = useAuthContext();
   const fullname = auth?.fullname ?? "ไม่พบชื่อในระบบ";
-  const { lineCtx } = useLineContext();
-  const pictureUrl =
-    lineCtx?.profile?.pictureUrl ??
-    "https://fastly.picsum.photos/id/962/200/200.jpg?hmac=XehF7z9JYkgC-2ZfSP05h7eyumIq9wNKUDoCLklIhr4";
-  const { auth } = useAuthContext();
-  const fullname = auth?.fullname ?? "ไม่พบชื่อในระบบ";
   return (
     <>
-      <ProfileBadge displayName={fullname} pictureUrl={pictureUrl} />
       <ProfileBadge displayName={fullname} pictureUrl={pictureUrl} />
       <section className="overflow-hidden px-4 sm:px-8 pt-content-top pb-content-bottom lg:px-16 lg:pb-content-bottom-lg lg:pt-content-top-lg">
         <Outlet />
