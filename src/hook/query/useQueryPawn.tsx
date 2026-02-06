e0bc900bdd79719432c73ede41e59d6a4aa5e00e
pongsathonwon <men1234-2008@hotmail.com>
2026-02-06 08:43:42 +0700

    Revert "revert this commit"
    This reverts commit f41f7907d9a32f16c1f3d726ac460c0ee23a2d13.

--- src/hook/query/useQueryPawn.tsx
+++ src/hook/query/useQueryPawn.tsx
@@ -14,18 +14,10 @@ function useQueryPawnById({ custCode }: TUseQueryPawnByIdProps) {
     refetchOnMount: true,
   });
   const canBePaid = q.data?.filter(({ pawnStatus }) =>
-    [
-      "due",
-      "due-soon",
-      "normal",
-      "overdue", // remove this line
-    ].includes(pawnStatus),
+    ["due", "due-soon"].includes(pawnStatus),
   );
   const cannotBePaid = q.data?.filter(({ pawnStatus }) =>
-    [
-      "remove this",
-      // "normal", "overdue"
-    ].includes(pawnStatus),
+    ["normal", "overdue"].includes(pawnStatus),
   );
   return { ...q, cannotBePaid, canBePaid };
 }
