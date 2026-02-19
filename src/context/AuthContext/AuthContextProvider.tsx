92a368cd5497b1c4a9e942f261c0c0dea961bc9a
pongsathonwon <men1234-2008@hotmail.com>
2026-02-19 10:52:25 +0700

    [fix] duplicate login call

--- src/context/AuthContext/AuthContextProvider.tsx
+++ src/context/AuthContext/AuthContextProvider.tsx
@@ -17,12 +17,10 @@ function AuthContextProvider({ children }: React.PropsWithChildren) {
   useEffect(() => {
     if (uid) {
       login(uid);
-      login(uid);
     }
   }, [uid]);
 
   useEffect(() => {
-    return registerAxiosTokenBearer();
     return registerAxiosTokenBearer();
   }, []);
 
